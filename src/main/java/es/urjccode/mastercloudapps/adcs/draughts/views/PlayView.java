2973abb66a8def14b6f9c783470554022da40b94
francois <fpoirier2002@gmail.com>
2019-11-07 23:02:35 +0100

    Step4 change parsing data

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
@@ -18,9 +18,8 @@ public class PlayView extends SubView {
         GameView gameView = new GameView();
         do {
             String command = this.console.readString("Mueven las " + color + ": ");
-            int origin = Integer.parseInt(command.substring(0, 2));
-            int target = Integer.parseInt(command.substring(3, 5));
-            error = playController.move(new Coordinate(origin/10-1, origin%10-1), new Coordinate(target/10-1, target%10-1));
+            String[] numbers = command.split("\\.|\\n");
+            error = playController.move(new Coordinate(numbers[0]),new Coordinate(numbers[1]));
             if (error != null){
             	error.writeln();
                 gameView.write(playController);
