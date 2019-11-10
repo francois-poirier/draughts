b13ff5aca4df9e5b1e915028f1a5a5468f707eee
francois <fpoirier2002@gmail.com>
2019-11-10 01:04:53 +0100

    Step18 clean code

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Piece.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Piece.java
@@ -14,37 +14,11 @@ public class Piece {
 		return this.color;
 	}
 
-	/*
-	 * protected Error isAdvanced(Coordinate origin, Coordinate target) { int
-	 * difference = origin.getRow() - target.getRow(); if (color == Color.WHITE){
-	 * return difference>0 ? null : Error.NOT_ADVANCED; } return difference<0 ? null
-	 * : Error.NOT_ADVANCED; }
-	 * 
-	 * protected Error isDiagonal(Coordinate origin , Coordinate target) { assert
-	 * target != null && target.isValid(); assert origin != null &&
-	 * origin.isValid(); boolean isDiagonal = origin.getRow() + origin.getColumn()
-	 * == target.getRow() + target.getColumn() || origin.getRow() -
-	 * origin.getColumn() == target.getRow() - target.getColumn(); return isDiagonal
-	 * ? null : Error.NOT_DIAGONAL; }
-	 * 
-	 * protected Error diagonalDistance(Coordinate origin, Coordinate target) {
-	 * assert target != null && target.isValid(); assert origin.isValid() &&
-	 * origin.isDiagonal(target); return Math.abs(origin.getRow() - target.getRow())
-	 * >= 3 ? Error.BAD_DISTANCE :null; }
-	 */
-
 	public Error isValidMovement(Coordinate origin, Coordinate target) {
-		/*
-		 * Error error = isDiagonal(origin, target); if (error != null) return error;
-		 * error = isAdvanced(origin, target); if (error != null) return error; error =
-		 * diagonalDistance(origin, target); if (error != null) return error;
-		 */
-		
 		Optional<Error> validate = new ChainedValidator().validate(origin, target, color);
 		if (validate.isPresent()) {
 			return validate.get();
-		}
-		
+		}	
 		return null;
 	}
 	    
